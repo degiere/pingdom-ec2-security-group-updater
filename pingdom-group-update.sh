e0a5432e3a2f6aab93674b81bb200b0c855fd6ec
Emil Kantis <e.kantis@gmail.com>
2016-05-30 15:29:29 +0200

    Added removal of old rules

--- pingdom-group-update.sh
+++ pingdom-group-update.sh
@@ -13,7 +13,22 @@ group=$1
 port=$2
 : ${port:="8080"}
 out=/tmp/pingdom.xml
+old_ips=/tmp/pingdom-old-ips.txt
+old_ports=/tmp/pingdom-old-ports.txt
 ips=/tmp/pingdom-ips.txt
+
+echo "Fetching old IPs in security group..."
+aws ec2 describe-security-groups --group-id sg-f0c07197 --query 'SecurityGroups[*].{Inbound:IpPermissions}' | grep FromPort | cut -d: -f2 | rev | cut -c 3- | rev > $old_ports
+aws ec2 describe-security-groups --group-id sg-f0c07197 --query 'SecurityGroups[*].{Inbound:IpPermissions}' | grep CidrIp | cut -d: -f2 | cut -c 3- | rev | cut -c 2- | rev > $old_ips
+
+old_ip_count=`cat $old_ips | wc -l`
+
+for i in `seq 1 $old_ip_count`;
+do
+	old_port=`sed "${i}q;d" $old_ports`
+	old_ip=`sed "${i}q;d" $old_ips`
+	aws ec2 revoke-security-group-ingress --group-id $group --cidr $old_ip --port $old_port --protocol tcp
+done
 
 echo "Fetching new pingdom probe ips..."
 curl https://my.pingdom.com/probes/feed > $out
